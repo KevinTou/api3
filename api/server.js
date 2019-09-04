8fbe81196912c8e72b0b7b73ab44737c06da51d7
Kevin Tou <toukevin@gmail.com>
2019-09-04 09:53:45 -0700

    Set up server and got it running

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.status(200).send({ api: "Up and running" });
+});
+
+// manage products (8)
+// manage clients (8)
+// manage orders
+// manage suppliers
+
+server.get("/products", (req, res) => {});
+server.get("/clients", (req, res) => {});
+server.get("/suppliers", (req, res) => {});
+
+module.exports = server;
